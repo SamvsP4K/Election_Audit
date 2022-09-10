ccd26533aaae286acd1633d4e8f923a068d014de
SamvsP4K <soundsofsamvs@gmail.com>
2022-09-10 14:16:42 -0700

    calculates percentage votes per candidate

--- pypoll.py
+++ pypoll.py
@@ -12,6 +12,7 @@ file_path = "resources/election_results.csv"
 #accumualtors
 total_votes = 0
 candidate_options = []
+candidate_votes = {}
 
 #opens file in read mode
 with open(file_path, "r") as election_data:
@@ -22,15 +23,26 @@ with open(file_path, "r") as election_data:
 
     #reads all rows in files
     for row in file_reader:
-        #adds total votes
+        #adds total votes and tracks vote count
         total_votes +=1
+        
         #adds unique candidate names
         candidate_name = row[2]
         if candidate_name not in candidate_options:
             candidate_options.append(candidate_name)
-       
-print(candidate_options)
-print(total_votes)
+            candidate_votes[candidate_name] = 0
+
+        candidate_votes[candidate_name] += 1
+            
+    for candidate_name in candidate_votes:
+        votes = candidate_votes[candidate_name]
+        vote_percentage = round(float(votes/total_votes) * 100,1)    
+        print(f"Candidate {candidate_name} recieved {vote_percentage} % of the vote")
+            
+# print(candidate_options)
+# print(total_votes)
+# print(candidate_votes)
+
 
 #writes to txt
 with open("analysis.txt","w") as txt:
