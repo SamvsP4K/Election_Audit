948ebc933510e875c76609823f0047ad1c9f06f8
SamvsP4K <soundsofsamvs@gmail.com>
2022-09-09 17:21:14 -0700

    Starter code for election audit

---
+++ pypoll.py
@@ -0,0 +1,18 @@
+#Data required:
+#1-The total number of votes cast
+#2-A complete list of candidates who receieved votes
+#3-the percentage of votews each candidate wonm
+#4-the total number of votes each candidate won
+#5-The winner based on popular vote
+
+import csv 
+
+#file path
+
+#opens file 
+election_data = open(file_to_load, "r")
+
+
+
+#closes file
+election_data.close()
